293426e0e3110dd0b086dfa72ac52e88ec03fcca
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2022-02-04 18:33:59 +0100

    esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
-<ul>
-    @foreach ($movies as $movie)
-        <li>{{$movie['title']}}</li>
-    @endforeach
-</ul>
+@extends('layout.layoutBase')
+
+@section('pageTitle')
+    Home    
+@endsection
+
+@section('pageContent')
+    <div class="main-content">
+        <h3>Lista Film</h3>
+        <div class="card-container">
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <h4>TITOLO: {{$movie['title']}}</h4>
+                    <h4>TITOLO ORIGINALE: {{$movie['original_title']}}</h4>
+                    <h5>NAZIONALITÀ: {{$movie['nationality']}}</h5>
+                    <h5>DATA: {{$movie['date']}}</h5>
+                    <h5>VOTO: {{$movie['vote']}}</h5>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="header-container">
+        <div class="header-left">
+            <h3>Movies</h3>
+        </div>
+        <div class="header-right">
+            <nav>
+                <ul>
+                    <li><a href="">sezione_1</a></li>
+                    <li><a href="">sezione_2</a></li>
+                    <li><a href="">sezione_3</a></li>
+                    <li><a href="">sezione_4</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+</header>
